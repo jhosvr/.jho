3670ae6848a7101c06ed98f9b4b03ee4e68ec9ba
Justin Ho <justinho@hosvr.com>
2020-02-02 02:09:12 -0500

    tools: kubectl sops helm terraform

---
+++ src/jho.sh
@@ -0,0 +1,58 @@
+# jho.sh
+# personal env preferences
+
+###
+### Functions
+###
+
+check_directory(){ 
+    if [[ ! -d "${1}" ]]; then mkdir -p "${1}"; fi; 
+}
+
+command_exists(){
+    which "${1}" &>/dev/null || { echo "The command '${1}' was not found in \$PATH" && exit 127; }
+}
+
+
+###
+### Script execution
+###
+
+# create directories
+check_directory "${HOME}/bin"
+check_directory "${HOME}/tmp"
+check_directory "${HOME}/.config"
+
+
+### Pulling preferred tools
+
+# check command dependencies
+command_exists wget
+command_exists tar
+command_exists unzip
+
+if [[ ! -d "${HOME}/tmp/jho" ]]; then mkdir -p "${HOME}/tmp/jho"; fi
+cd "${HOME}/tmp/jho"
+echo -n "Pulling tools: "
+# gets binaries from 2nd field of tools.txt file
+for line in $(cat ${HOME}/repos/.jho/src/tools.txt); do
+    tool="$(echo ${line} | cut -d';' -f1)"
+    url="$(echo ${line} | cut -d';' -f2)"
+    echo -n "${tool} "
+    wget -q "${url}" -O "${tool}"
+
+    # handle non binary downloads
+    case "${tool}" in
+        "helm")
+            tar -xzf helm
+            tool="linux-amd64/helm" ;;
+        "terraform")
+            unzip -oq terraform ;;
+    esac
+    chmod +x "${tool}"
+    cp "${tool}" "${HOME}/bin/" ;
+done
+echo
+
+# clean out tmp folder
+rm -rf "${HOME}/tmp/jho"
